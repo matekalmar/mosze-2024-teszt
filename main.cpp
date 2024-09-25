3cb0bd85b92db416a4fad9738bad8b3967ddf5bc
matekalmar <matekalmar6@gmail.com>
2024-09-25 11:45:09 +0200

    Hibák a main.cpp-ben
    Kommenteltem a fellelt hibákat

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //'' helyett "" és a sor végén hiányzó ;
+    for (int i = 0;) //helytelen for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //helytelen for ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányoznak az értékek és a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // atlag erteke legyen 0
+    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző
     {
-        atlag += b[i]
+        atlag += b[i] //pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    //az allokált memóriát fel kell szabadítani
     return 0;
 }
