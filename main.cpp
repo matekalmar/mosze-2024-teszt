1b3206a0f5aae642f8fb782f5d40f9050183cb52
matekalmar <matekalmar6@gmail.com>
2024-09-28 17:12:08 +0200

    main.cpp javított

--- main.cpp
+++ main.cpp
@@ -4,25 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[N_ELEMENTS]; //NELEMENTS helyett N_ELEMENTS
-    std::cout << "1-100 ertekek duplazasa"; //'' helyett "" és a sor végén hiányzó ;
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i < N_ELEMENTS; i++) //helytelen for ciklus
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" << b[i] << "\n"; //hiányoznak az értékek és a ;
+        std::cout << "Ertek:" << b[i] << "\n";
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag = 0; // atlag erteke legyen 0
-    for (int i = 0; i < N_ELEMENTS; i++) //vessző helyett pontosvessző
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-       atlag += b[i]; //pontosvessző hiányzik
+       atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    //az allokált memóriát fel kell szabadítani
     delete[] b;
     return 0;
 }
