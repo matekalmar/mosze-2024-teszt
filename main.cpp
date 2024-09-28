223951399bb4c557def7807528b6f3398bfb492a
matekalmar <matekalmar6@gmail.com>
2024-09-28 17:09:18 +0200

    Kijavított main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' //'' helyett "" és a sor végén hiányzó ;
-    for (int i = 0;) //helytelen for ciklus
+    int *b = new int[N_ELEMENTS]; //NELEMENTS helyett N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa"; //'' helyett "" és a sor végén hiányzó ;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) //helytelen for ciklus
+    for (int i = 0; i < N_ELEMENTS; i++) //helytelen for ciklus
     {
-        std::cout << "Ertek:" //hiányoznak az értékek és a ;
+        std::cout << "Ertek:" << b[i] << "\n"; //hiányoznak az értékek és a ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // atlag erteke legyen 0
-    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző
+    int atlag = 0; // atlag erteke legyen 0
+    for (int i = 0; i < N_ELEMENTS; i++) //vessző helyett pontosvessző
     {
-        atlag += b[i] //pontosvessző hiányzik
+       atlag += b[i]; //pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     //az allokált memóriát fel kell szabadítani
+    delete[] b;
     return 0;
 }
